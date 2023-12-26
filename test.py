e5c137481d227b503930ddefd6c1ca2acdf03e41
Kain7f1 <kain7f1@gmail.com>
2023-12-27 08:21:35 +0900

    logs 파일 합칠때, 2종류의 파일을 만듦
    - 1. 숫자 데이터를 더한 것 : 전체 총 데이터 양, 에러 양, 총 걸린 시간을 확인할 때 사용한다
    - 2. 1000단위의 row를 붙인 것 : 크롤링 진행 데이터를 분석할 때 사용한다

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
+from news_crawler import crawl_url_recursion, crawl_text, crawl_text_specifying_index
 import utility_module as util
 import crawling_tool as cr
-from bs4 import BeautifulSoup
-
-url_1 = "https://n.news.naver.com/mnews/article/087/0000158119?sid=102"
-url_2 = "https://n.news.naver.com/mnews/article/215/0000491837?sid=101"
-url_3 = "https://n.news.naver.com/mnews/article/032/0003256030?sid=101"
 
 ##########
 search_keyword = "금리"
-# cr.merge_text_temp_files(search_keyword)
+cr.merge_text_temp_files(search_keyword)
 
 #######################
 # 합치기
@@ -19,7 +15,12 @@ search_keyword = "금리"
 ##################
 # 결손 데이터가 있는지 체크
 # 예시 사용법
-directory = "./text_1/temp_results"  # 'temp_results' 폴더 경로 지정
-different_chunk_size_index = cr.find_different_chunk_size(directory)
-print(different_chunk_size_index)
-
+# directory = "./text_1/temp_results"  # 'temp_results' 폴더 경로 지정
+# different_chunk_size_index = cr.find_different_chunk_size(directory)
+# print(different_chunk_size_index)
+#
+#
+# ###########################################
+# # 수동 크롤링
+# index_list = [61, 364, 551, 585, 658, 680, 793, 796, 804, 851, 852, 864]
+# crawl_text_specifying_index(search_keyword, index_list)
